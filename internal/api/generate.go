e3a6ff2889ad6977442d1d4a97777e791e93050c
piq <paul1.decauchy@epitech.eu>
2026-02-02 17:23:58 +0100

    feat: init ogen

--- internal/api/generate.go
+++ internal/api/generate.go
@@ -2,4 +2,4 @@ package api
 
 //go:generate redocly bundle server.yaml -o bundled/server.yaml --config config.yaml
 //go:generate redocly bundle server.yaml -o bundled/swagger.yaml
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen --target generated -package generated --clean bundled/server.yaml
+//go:generate ogen --target generated -package generated --clean bundled/server.yaml
